6b42cc93695a8581c5d263399237e2cfcd4ad689
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-04-14 19:38:35 +0900

    add Result and css

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,47 @@ import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Results';
 import './App.css';
+import { useState } from 'react';
+
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperatuere: string;
+  conditionText: string;
+  icon: string;
+}
+
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperatuere: "",
+    conditionText: "",
+    icon: ""
+  });
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      fetch(`https://api.weatherapi.com/v1/current.json?key=59c2875b30e749b9ab590336231304&q=${city}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperatuere: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon
+        })
+      })
+  }
   return (
-    <div className="App">
-      <Title/>
-      <Form/>
-      <Results/>
+    <div className="wrapper">
+      <div className='container'>
+        <Title/>
+        <Form setCity={setCity} getWeather={getWeather}/>
+        <Results results={results}/>
+      </div>
     </div>
   );
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,26 @@
-const Results = () => {
-    return (
-        <h1>気象データ</h1>
+type ResultsPropsType = {
+    results: {
+        country: string;
+        cityName: string;
+        temperatuere: string;
+        conditionText: string;
+        icon: string;
+    }
+}
 
+const Results = (props: ResultsPropsType) => {
+    return (
+        <div>
+            {props.results.country && <div>{props.results.country}</div>}
+            {props.results.cityName && <div>{props.results.cityName}</div>}
+            {props.results.temperatuere && <div>{props.results.temperatuere}<span>°C</span></div>}
+            {props.results.conditionText && 
+                <div>
+                    <img src={props.results.icon} alt="icon"/>
+                    <span>{props.results.conditionText}</span>
+                </div>
+            }
+        </div>
     );
 };
 
